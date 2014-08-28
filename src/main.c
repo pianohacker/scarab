7435a58274527a86d780ce5520312963f77fb258
Jesse Weaver <pianohacker@gmail.com>
2014-08-28 13:20:28 -0600

    Documentation, extra example

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+// # Headers
 #include <stdbool.h>
 #include <stdio.h>
 
@@ -6,16 +7,25 @@
 #include "parser.h"
 #include "value.h"
 
+// # Main program
 int main(int argc, char **argv) {
+	// Start up the execution context, where the root scope and other information specific to this
+	// interpreter lives.
 	KhContext *ctx = kh_context_new();
 
+	// ## File execution
+	// Check for a filename as the first argument
+
+	// ## REPL
 	while (true) {
-		// Memory is free, laziness is rewarding
+		// Use a large input buffer; memory is cheap, laziness is rewarding.
 		char buffer[65536];
 		printf("> ");
 
+		// Check for EOF.
 		if (!fgets(buffer, sizeof(buffer), stdin)) break;
 
+		// Parse our input string into a list of lists (assumed to be an open list).
 		GError *err = NULL;
 		KhValue *forms = kh_parse_string(buffer, &err);
 
@@ -24,11 +34,13 @@ int main(int argc, char **argv) {
 			continue;
 		}
 
+		// Only print a number before each result if there is more than one result.
 		bool print_number = true;
 		if (forms->d_right == kh_nil) {
 			print_number = false;
 		}
 
+		// Finally, run each form, checking for errors, and print out the result.
 		int i = 1;
 		KH_ITERATE(forms) {
 			KhValue *value = kh_eval(ctx, elem->d_left);
@@ -37,6 +49,7 @@ int main(int argc, char **argv) {
 				printf("Error: %s\n", kh_inspect(kh_get_error(ctx)));
 			} else {
 				if (print_number) printf("%d. ", i++);
+				// If we run only a single form and it returns nil, don't bother printing it.
 				if (print_number || value != kh_nil) puts(kh_inspect(value));
 			}
 		}
