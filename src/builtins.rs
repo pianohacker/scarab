e7612279c6643f9e9753629acd2b0bce2a8c20e1
Jesse Weaver <pianohacker@gmail.com>
2021-11-03 01:33:19 -0600

    Minor cleanup

--- src/builtins.rs
+++ src/builtins.rs
@@ -11,13 +11,11 @@ use crate::vm::code;
 use crate::vm::Vm;
 
 pub(crate) struct Builtin {
-    pub num_outputs: code::RegisterOffset,
     pub run: &'static (dyn Fn(&mut Vm<'_>) + Sync),
 }
 
 static BUILTINS: phf::Map<&'static str, Builtin> = phf_map! {
     "+" => Builtin {
-        num_outputs: 1,
         run: &|vm| {
             vm.registers[0] = Value::Integer(
                 vm.registers.iter().map(|v| v.as_isize().unwrap()).sum(),
@@ -25,7 +23,6 @@ static BUILTINS: phf::Map<&'static str, Builtin> = phf_map! {
         },
     },
     "-" => Builtin {
-        num_outputs: 1,
         run: &|vm| {
             vm.registers[0] = Value::Integer(
                 vm.registers.iter().map(|v| v.as_isize().unwrap()).reduce(|a, b| a -b).unwrap(),
@@ -33,10 +30,11 @@ static BUILTINS: phf::Map<&'static str, Builtin> = phf_map! {
         },
     },
     "debug" => Builtin {
-        num_outputs: 0,
         run: &|vm| {
             let output: Vec<_> = vm.registers.iter().map(|v| format!("{}", v)).collect();
             write!(vm.debug_output, "{}\n", output.join(" ")).unwrap();
+
+            vm.registers[0] = Value::Nil;
         }
     },
 };
